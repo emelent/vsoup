15db81517a6be19844eb4ec0189f38ed3bc85745
Roy of Earth <technicmk@gmail.com>
2017-09-17 03:12:48 +0200

    Update gql resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,81 +1,67 @@
 const ObjectId = require('mongoose').Types.ObjectId
+
+const gqlEvent = x => {
+	x._id = x._id.toString()
+	if(x.module_id)
+		x.module_id = x.module_id.toString()
+	if (x.date)
+		x.date = x.date.toString()
+	if (x.author_id)
+		x.author_id = x.author_id.toString()
+
+	return x
+}
+const gqlModule = x => {
+	x._id = x._id.toString()
+	return  x
+}
+
 module.exports = {
 	Query: {
 		modules: async(parent, args, {
 			Module
 		}) => {
 			const modules = await Module.find(args)
-			return modules.map(x => {
-				x._id = x._id.toString()
-				return x
-			})
+			return modules.map(x => gqlModule(x))
 		},
 		events: async(parent, args, {
 			Event
 		}) => {
 			const events = await Event.find(args)
-			return events.map(x => {
-				x._id = x._id.toString()
-				x.module_id = x.module_id.toString()
-				if (x.date)
-					x.date = x.date.toString()
-				if (x.author_id)
-					x.author_id = x.author_id.toString()
-
-				return x
-			})
+			return events.map(x => gqlEvent(x))
 		},
 	},
 	Mutation: {
 		createModule: async (parent, args, {Module}) => {
 			const x = await new Module(args).save()
-			x._id = x._id.toString()
-			return x
+			return gqlModule(x)
 		},
 		updateModule: async (parent, args, {Module}) => {
 			const _id = ObjectId.createFromHexString(args._id)
 			delete args._id
 			const x = await Module.findOneAndUpdate({_id},args)
-			x._id = x._id.toString()
-			return x
+			return gqlModule(x)
 		},
 		deleteModule: async (parent, args, {Module}) => {
 			const _id = ObjectId.createFromHexString(args._id)
 			const x = await Module.findOneAndDelete({_id})
-			x._id = x._id.toString()
-			return x
+			return gqlModule(x)
 		},
 
 		createEvent: async (parent, args, {Event}) => {
 			const x = await new Event(args).save()
-			x._id = x._id.toString()
-			x.module_id = x.module_id.toString()
-			if (x.date)
-				x.date = x.date.toString()
-			if (x.author_id)
-				x.author_id = x.author_id.toString()
-			return x
+			return gqlEvent(x)
 		},
 		updateEvent: async (parent, args, {Event}) => {
 			const _id = ObjectId.createFromHexString(args._id)
 			delete args._id
 			const x = await Event.findOneAndUpdate({_id},args)
-			x._id = x._id.toString()
-			if (x.date)
-				x.date = x.date.toString()
-			if (x.author_id)
-				x.author_id = x.author_id.toString()
-			return x
+			return gqlEvent(x)
 		},
 		deleteEvent: async (parent, args, {Event}) => {
 			const _id = ObjectId.createFromHexString(args._id)
 			const x = await Event.findOneAndDelete({_id})
-			x._id = x._id.toString()
-			if (x.date)
-				x.date = x.date.toString()
-			if (x.author_id)
-				x.author_id = x.author_id.toString()
-			return x
+			return gqlEvent(x)
 		},
 	},
 };
