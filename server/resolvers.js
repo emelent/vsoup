1ae9ccdd0896f0709d0e1fc12bf09c34e8928034
Roy of Earth <technicmk@gmail.com>
2017-09-18 19:44:06 +0200

    Fix up minor bugs in resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,7 @@
 const ObjectId = require('mongoose').Types.ObjectId
 
 const gqlEvent = x => {
+	if(!x) return
 	x._id = x._id.toString()
 	if(x.module_id)
 		x.module_id = x.module_id.toString()
@@ -12,6 +13,7 @@ const gqlEvent = x => {
 	return x
 }
 const gqlModule = x => {
+	if(!x) return
 	x._id = x._id.toString()
 	return  x
 }
@@ -19,21 +21,20 @@ const gqlModule = x => {
 module.exports = {
 	Query: {
 		modules: async(parent, args, {Module}) => {
-			const modules = await Module.find(args)
-			console.log('Results =>', modules.map(x => gqlModule(x)))
+			const modules = await Module.find(args).exec()
 			return modules.map(x => gqlModule(x))
 		},
 		module: async(parent, args, {Module}) => {
-			const x = await Module.findOne(args)
+			const x = await Module.findOne(args).exec()
 			return gqlModule(x)
 		},
 
 		events: async(parent, args, {Event}) => {
-			const events = await Event.find(args)
+			const events = await Event.find(args).exec()
 			return events.map(x => gqlEvent(x))
 		},
 		event: async(parent, args, {Event}) => {
-			const x = await Event.findOne(args)
+			const x = await Event.findOne(args).exec()
 			return gqlEvent(x)
 		},
 		
@@ -46,12 +47,13 @@ module.exports = {
 		updateModule: async (parent, args, {Module}) => {
 			const _id = ObjectId.createFromHexString(args._id)
 			delete args._id
-			const x = await Module.findOneAndUpdate({_id},args)
+			const x = await Module.findByIdAndUpdate(_id,args).exec()
+			console.log(x)
 			return gqlModule(x)
 		},
 		deleteModule: async (parent, args, {Module}) => {
 			const _id = ObjectId.createFromHexString(args._id)
-			const x = await Module.findOneAndDelete({_id})
+			const x = await Module.findByIdAndRemove({_id}).exec()
 			return gqlModule(x)
 		},
 
@@ -62,12 +64,12 @@ module.exports = {
 		updateEvent: async (parent, args, {Event}) => {
 			const _id = ObjectId.createFromHexString(args._id)
 			delete args._id
-			const x = await Event.findOneAndUpdate({_id},args)
+			const x = await Event.findByIdAndUpdate(_id,args).exec()
 			return gqlEvent(x)
 		},
 		deleteEvent: async (parent, args, {Event}) => {
 			const _id = ObjectId.createFromHexString(args._id)
-			const x = await Event.findOneAndDelete({_id})
+			const x = await Event.findByIdAndRemove(_id).exec()
 			return gqlEvent(x)
 		},
 	},
