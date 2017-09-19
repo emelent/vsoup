55141802f880c90c7d1261dfcabfd7a9766a3487
Roy of Earth <technicmk@gmail.com>
2017-09-19 07:50:28 +0200

    Rename file

---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
+
+const TimetableAlias = new mongoose.Schema({
+	timetable_id: ObjectId,
+	author_id: ObjectId
+})
+
+const schema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	student_id: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	group: {
+		type: String,
+		default: 'STUDENT',
+		uppercase: true
+	},
+	modules: [ObjectId],
+	timetables: [ObjectId],
+	timetable_aliases: [TimetableAlias]
+})
+
+module.exports = mongoose.model('User', schema)
