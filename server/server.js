538881e0601a3e2a66e5ea4c0c4d8202f89dad4d
Roy of Earth <technicmk@gmail.com>
2017-09-19 23:27:40 +0200

    Add request object to gql context

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {makeExecutableSchema} = require('graphql-tools')
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
 const models = require('./models')
+
 const schema = makeExecutableSchema({
 	typeDefs,
 	resolvers
@@ -18,9 +19,17 @@ mongoose.connect('mongodb://localhost/mydb', {useMongoClient: true})
 const app = express()
 const port = 5000
 const db = mongoose.connection
+const authRouter = require('./auth')(db, models.User)
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context: {...models} }));
+app.use('/graphql', 
+	bodyParser.json(), 
+	graphqlExpress(req => ({
+		schema, 
+		context: {...models, req}
+	}))
+);
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use('/auth',  authRouter)
 
 app.listen(port, () => {
 	console.log(`Listening on :${port}`)
