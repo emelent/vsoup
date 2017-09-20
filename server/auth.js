d7bbbf100c6f7c464e977f1b1ada793ecc805388
Roy of Earth <technicmk@gmail.com>
2017-09-20 13:18:33 +0200

    Implement auth routes

--- server/auth.js
+++ server/auth.js
@@ -1,50 +1,62 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const {createToken, validateToken, hashPassword} = require('./utils')
+const {createToken, validateToken, hashPassword, inflateId} = require('./utils')
 
 const expiresIn = '2d'
-const newToken = (user, req) => {
-	return createToken({
-		_id: user._id.toString(),
-		ua: req.headers['user-agent']
-	}, {expiresIn})
-}
+
+const fail = (res, code, msg) => res.status(code).json(msg)
+
+const newToken = (_id, ua) => ({token: createToken({_id, ua}, {expiresIn})})
 const makeRouter = (db, User) => {
 	const router = express.Router()
 
 	router.use(bodyParser.json())
 
-	router.all('/', (req, res) => {
-		console.log('body =>', req.body)
-
-		res.json(req.headers)
-	})
 	router.post('/token', async (req, res) => {
-		if(!req.body.password || !req.body.student_id){
-			res.status(422).json("No password or student_id provided.")
-			return		
-		}
+		if(!req.body.password || !req.body.student_id)
+			return fail(res, 422, "No password or student_id provided.")
 		req.body.password = hashPassword(req.body.password)
 		const user = await User.findOne(req.body).exec()
-		if(!user){
-			res.status(403).json("Invalid login credentials.")
-			return
-		}
-		
-		res.json({token: newToken(user, req)})
+		if(!user)
+			fail(res, 403, "Invalid login credentials.")
+		const _id = user._id.toString()
+		const ua = req.get('user-agent')
+		res.json(newToken(_id, ua))
 	})
 	router.post('/register', async (req, res) => {
 		req.body.password = hashPassword(req.body.password)
 		const user = await new User(req.body).save()
-		if(!user){
-			res.status(401).json("Hmmm... what are you trying to do?")
-			return		
-		}
-
-		res.json({token: newToken(user, req)})
+		if(!user)
+			return fail(res, 401, "Hmmm... what are you trying to do?")
+		const _id = user._id.toString()
+		const ua = req.get('user-agent')
+		
+		res.json(newToken(_id, ua))
 	})
-	router.post('/refresh', (req, res) => {
-	
+	router.all('/refresh', async (req, res) => {
+		const ua = req.get('user-agent')
+		const bearer = req.get('authorization')
+		const badToken = () => fail(res, 403, "Invalid token.")
+
+		if(!bearer)
+			return badToken()
+			
+		const oldToken = bearer.split(' ')[1]
+		const payload = validateToken(oldToken, {ignoreExpiration: true})
+
+		if(!payload || payload.ua !== ua)
+			return badToken()
+
+		const user = await User.findById(inflateId(payload._id))
+		if(!user)
+			return badToken()
+
+		delete payload.iat
+		delete payload.exp
+		delete payload.nbf
+		delete payload.jti 
+
+		res.json(newToken(payload._id, ua))
 	})	
 
 	return router
