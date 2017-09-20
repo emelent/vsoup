64e94a79ac04866eca8be142281154a43e2acae8
Roy of Earth <technicmk@gmail.com>
2017-09-20 15:16:33 +0200

    Include Venue model

--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ module.exports = {
 	Event: require('./event'),
 	Module: require('./module'),
 	Timetable: require('./timetable'),
-	User: require('./user')
+	User: require('./user'),
+	Venue: require('./venue')
 }
